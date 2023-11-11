7344cee923030f9032f4f07e5b66ef0f09e7856a
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-11 15:03:50 +0300

    Add testing slice

--- slices/sum.go
+++ slices/sum.go
@@ -10,6 +10,29 @@ func Sum(numbers []int) int {
 	return sum
 }
 
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSumTails ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSumTails {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
+
 func main() {
 
 }
