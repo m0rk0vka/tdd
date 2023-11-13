17d7303fbdfc1ecd3f1d74ec2b9c9f707cdfe3d7
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-13 19:52:59 +0300

    Add goroutines to provide speed

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockingWebsiteChecker(url string) bool {
@@ -24,6 +25,21 @@ func TestCheckWebsites(t *testing.T) {
 	}
 
 	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, want %v", got, want)
+		t.Errorf("got %#v, want %#v", got, want)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < 100; i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
