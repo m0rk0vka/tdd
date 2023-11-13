313e7ca956916943a6524ae58841bb7d075bdcb3
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-13 14:54:38 +0300

    Move some structs to test file

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -25,3 +25,26 @@ Go!`
 		t.Errorf("Not enough calls for sleeper, want 4 got %d", s.Calls)
 	}
 }
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
