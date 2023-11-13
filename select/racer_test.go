88cce75df5ae4acd49483b84d99ab1a808a0113c
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-13 20:38:55 +0300

    Racer func with test, httptest

---
+++ select/racer_test.go
@@ -0,0 +1,33 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(10 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	got := Racer(slowURL, fastURL)
+
+	want := fastURL
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
