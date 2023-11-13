e6ed918115275daa304f5b3d034b1892ceb53720
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-13 14:33:28 +0300

    Add tdd for countdown func

---
+++ mocking/countdown_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountDown(t *testing.T) {
+	b := &bytes.Buffer{}
+
+	Countdown(b)
+
+	got := b.String()
+	want := `3
+2
+1
+Go!`
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
---
+++ mocking/go.mod
@@ -0,0 +1,3 @@
+module tdd/mocking
+
+go 1.21.4
