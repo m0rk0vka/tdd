75bbceed3ec02a7faa84aacb65026b3117518547
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-14 14:18:46 +0300

    Add basic test and implementation counter

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,1 +1,20 @@
 package sync
+
+import "testing"
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing counter 3 times leaves it at 3", func(t *testing.T) {
+		c := Counter{}
+		c.Inc()
+		c.Inc()
+		c.Inc()
+		assertCounter(t, c, 3)
+	})
+}
+
+func assertCounter(t testing.TB, c Counter, wantValue int) {
+	t.Helper()
+	if c.Value() != 3 {
+		t.Errorf("got %d, want %d", c.Value(), 3)
+	}
+}
