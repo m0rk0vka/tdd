badbe4b8334a9d3b8b82feba1b23035974b9998e
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-14 14:30:13 +0300

    Add mutex to concurrency access

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,20 +1,41 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing counter 3 times leaves it at 3", func(t *testing.T) {
-		c := Counter{}
+		c := NewCounter()
 		c.Inc()
 		c.Inc()
 		c.Inc()
 		assertCounter(t, c, 3)
 	})
+	t.Run("run safety concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		c := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				defer wg.Done()
+				c.Inc()
+			}()
+		}
+
+		wg.Wait()
+
+		assertCounter(t, c, wantedCount)
+	})
 }
 
-func assertCounter(t testing.TB, c Counter, wantValue int) {
+func assertCounter(t testing.TB, c *Counter, wantValue int) {
 	t.Helper()
-	if c.Value() != 3 {
-		t.Errorf("got %d, want %d", c.Value(), 3)
+	if c.Value() != wantValue {
+		t.Errorf("got %d, want %d", c.Value(), wantValue)
 	}
 }
