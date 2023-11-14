3ca9fa8ffbd33507b38a6a3f42b936d39d5ed325
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-14 14:50:17 +0300

    Add basic scenario for server

--- context/context_test.go
+++ context/context_test.go
@@ -1,1 +1,29 @@
 package context
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type subStore struct {
+	response string
+}
+
+func (s subStore) Fetch() string {
+	return s.response
+}
+
+func TestServer(t *testing.T) {
+	data := "Hello, World!"
+	svr := Server(&subStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+	}
+}
