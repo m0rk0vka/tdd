3c933402da6e4f67f06ac23ca5ee95cb977b2151
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-14 13:28:17 +0300

    Make test with map pass

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -7,26 +7,25 @@ import (
 func walk(x interface{}, fn func(string)) {
 	v := getValue(x)
 
-	numberOfValues := 0
-	var getField func(int) reflect.Value
+	walkValue := func(v reflect.Value) {
+		walk(v.Interface(), fn)
+	}
 
 	switch v.Kind() {
 	case reflect.String:
 		fn(v.String())
 	case reflect.Slice, reflect.Array:
-		numberOfValues = v.Len()
-		getField = v.Index
+		for i := 0; i < v.Len(); i++ {
+			walkValue(v.Index(i))
+		}
 	case reflect.Struct:
-		numberOfValues = v.NumField()
-		getField = v.Field
+		for i := 0; i < v.NumField(); i++ {
+			walkValue(v.Field(i))
+		}
 	case reflect.Map:
 		for _, key := range v.MapKeys() {
-			walk(v.MapIndex(key).Interface(), fn)
+			walkValue(v.MapIndex(key))
 		}
-	}
-
-	for i := 0; i < numberOfValues; i++ {
-		walk(getField(i).Interface(), fn)
 	}
 }
 
