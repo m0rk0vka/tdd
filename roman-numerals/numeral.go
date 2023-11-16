00713004b6033b538cc1224475c7178cfb7f3f54
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-16 13:29:49 +0300

    Pass test for IX

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -3,23 +3,23 @@ package romannumerals
 import "strings"
 
 func ConvertToRoman(arabic int) string {
-
-	if arabic == 4 {
-		return "IV"
-	}
-
 	var result strings.Builder
 
-	for i := arabic; i > 0; i-- {
-		if i == 5 {
+	for arabic > 0 {
+		switch {
+		case arabic > 8:
+			result.WriteString("IX")
+			arabic -= 9
+		case arabic > 4:
 			result.WriteString("V")
-			break
-		}
-		if i == 4 {
+			arabic -= 5
+		case arabic > 3:
 			result.WriteString("IV")
-			break
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic -= 1
 		}
-		result.WriteString("I")
 	}
 	return result.String()
 }
